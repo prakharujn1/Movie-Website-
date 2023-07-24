7d98f8334a8be7750a789946b6c60646436ab329
prakharujn1 <128589011+prakharujn1@users.noreply.github.com>
2023-07-24 20:00:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,31 +4,31 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-// ye HTML WALE TAG
+ 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-///initalyy get fav movies
+ 
 getMovies(APIURL);
 
 async function getMovies(url) {
   const resp = await fetch(url);
   const respData = await resp.json();
 
-  // movie aa gyi
+   
   console.log(respData);
-  // yaha pe show karenge
+ 
   showMovies(respData.results);
 
 }
 
 function showMovies(movies) {
-  //clear main
+  
   main.innerHTML = "";
   movies.forEach((movie) => {
     const { poster_path, title, vote_average, overview } = movie;
-    // raja
+  
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
 
